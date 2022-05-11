ca63fb1012491acd5a15ff8a083e699106d884f5
Andres Agudelo <anagudelogu@gmail.com>
2022-05-11 16:16:29 -0500

    Add addScore method

--- src/modules/leaderboard.js
+++ src/modules/leaderboard.js
@@ -1,5 +1,4 @@
 import FetchRequest from './fetchRequest.js';
-import Score from './score.js';
 
 export default class Leaderboard {
 	static async getScores({ url }) {
@@ -7,6 +6,23 @@ export default class Leaderboard {
 			const fetchRequest = new FetchRequest({ url });
 			const data = await fetchRequest.call();
 			return data.result;
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	static async addScore({ name, score }, url) {
+		try {
+			const fetchRequest = new FetchRequest({
+				method: 'POST',
+				url: url,
+				body: {
+					user: name,
+					score: score,
+				},
+			});
+			const data = await fetchRequest.call();
+			return data;
 		} catch (error) {
 			console.error(error);
 		}
