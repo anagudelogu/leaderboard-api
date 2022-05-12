4a11273f2e6451f1fd39120a7f76a66c9f2dd72d
Andres Agudelo <anagudelogu@gmail.com>
2022-05-11 20:35:57 -0500

    Add event listeners to Window load and buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import './styles.css';
+import Utility from './modules/utility';
+import UserInterface, { LIST } from './modules/userInterface';
+
+const refreshBtn = document.querySelector(
+	'.leaderboard__header--btn'
+);
+const form = document.querySelector('form');
+
+window.addEventListener('load', async () => {
+	try {
+		const displayScores = await UserInterface.displayScores();
+		return displayScores;
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+refreshBtn.addEventListener('click', async () => {
+	try {
+		LIST.innerHTML = '';
+		const displayScores = await UserInterface.displayScores();
+		return displayScores;
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+form.addEventListener('submit', async (e) => {
+	try {
+		e.preventDefault();
+		const [name, score] = Array.from(form.elements);
+		const createNewScore = await Utility.createNewScoreAPI({
+			name: name.value,
+			score: score.value,
+		});
+		name.value = '';
+		score.value = '';
+		return createNewScore;
+	} catch (error) {
+		console.error(error);
+	}
+});
