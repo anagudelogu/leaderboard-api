4006b86ac84f0b09e8faa95ef08b985ad8f632cc
Andres Agudelo <anagudelogu@gmail.com>
2022-05-12 06:40:02 -0500

    Remove await from non promise related functions on Utility class

--- src/modules/utility.js
+++ src/modules/utility.js
@@ -3,20 +3,16 @@ import Score from './score.js';
 import Leaderboard from './leaderboard.js';
 
 export default class Utility {
-  static async getUrl() {
-    try {
-      const gameId = gameObj.id;
-      const gameUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`;
-      return gameUrl;
-    } catch (error) {
-      throw new Error(error);
-    }
+  static getUrl() {
+    const gameId = gameObj.id;
+    const gameUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`;
+    return gameUrl;
   }
 
   static async createNewScoreAPI({ name, score }) {
     try {
       const scr = new Score({ name, score });
-      const gameUrl = await Utility.getUrl();
+      const gameUrl = Utility.getUrl();
       const addScore = await Leaderboard.addScore(scr, gameUrl);
       return addScore;
     } catch (error) {
