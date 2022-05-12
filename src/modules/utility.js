962bcf34b9dfad432a0ee1eb4f960c38384a4488
Andres Agudelo <anagudelogu@gmail.com>
2022-05-11 20:27:49 -0500

    Refactored Utility class to function with the new fixed id

--- src/modules/utility.js
+++ src/modules/utility.js
@@ -1,21 +1,26 @@
-import Game from './game';
+import { gameObj } from './game';
 import Score from './score';
 import Leaderboard from './leaderboard';
 
 export default class Utility {
-	static async startGameAndGetUrl() {
-		const game = new Game({ name: 'My game' });
-		const gameId = await game.getGameId();
-		const gameUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`;
-		return gameUrl;
+	static async getUrl() {
+		try {
+			const gameId = gameObj.id;
+			const gameUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`;
+			return gameUrl;
+		} catch (error) {
+			console.error(error);
+		}
 	}
 
 	static async createNewScoreAPI({ name, score }) {
-		const scr = new Score({ name: name, score: score });
-		console.log(scr);
-		const gameUrl = await Utility.startGameAndGetUrl();
-		const addScore = await Leaderboard.addScore(scr, gameUrl);
-		console.log(addScore);
-		return addScore;
+		try {
+			const scr = new Score({ name: name, score: score });
+			const gameUrl = await Utility.getUrl();
+			const addScore = await Leaderboard.addScore(scr, gameUrl);
+			return addScore;
+		} catch (error) {
+			console.error(error);
+		}
 	}
 }
