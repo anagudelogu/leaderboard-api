9ce64affa3c0989c6e30164b4007396c50bded6c
Andres Agudelo <anagudelogu@gmail.com>
2022-08-28 19:59:20 -0500

    Update index with event listener to use new methods

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles.css';
-import Utility from './modules/utility.js';
-import UserInterface, { LIST } from './modules/userInterface.js';
+import { createScore } from './services/leaderboardApi.js';
+import UserInterface, { list } from './modules/userInterface.js';
+import { addIconToFirstScore } from './helpers/scoreHelpers.js';
 
 const refreshBtn = document.querySelector(
   '.leaderboard__header--btn',
@@ -9,36 +10,35 @@ const form = document.querySelector('form');
 
 window.addEventListener('load', async () => {
   try {
-    const displayScores = await UserInterface.displayScores();
-    UserInterface.addIconToFirstScore();
-    return displayScores;
+    await UserInterface.displayScores();
+    addIconToFirstScore();
   } catch (error) {
     throw new Error(error);
   }
 });
 
 refreshBtn.addEventListener('click', async () => {
+  list.innerHTML = '';
   try {
-    LIST.innerHTML = '';
-    const displayScores = await UserInterface.displayScores();
-    UserInterface.addIconToFirstScore();
-    return displayScores;
+    await UserInterface.displayScores();
+    addIconToFirstScore();
   } catch (error) {
     throw new Error(error);
   }
 });
 
 form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const [name, score] = Array.from(form.elements);
+
   try {
-    e.preventDefault();
-    const [name, score] = Array.from(form.elements);
-    const createNewScore = await Utility.createNewScoreAPI({
-      name: name.value,
+    await createScore({
+      user: name.value,
       score: score.value,
     });
+
     name.value = '';
     score.value = '';
-    return createNewScore;
   } catch (error) {
     throw new Error(error);
   }
